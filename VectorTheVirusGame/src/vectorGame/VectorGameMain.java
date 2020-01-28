ebdf7e76f49a3ec3bc03ee586efb9f7d8720aaea
Jack.Martin <Jack.Martin@MU-C1-11.mullen.pvt.k12.co.us>
2020-01-28 11:57:36 -0700

    Outline

---
+++ VectorTheVirusGame/src/vectorGame/VectorGameMain.java
@@ -0,0 +1,33 @@
+package vectorGame;
+
+public class VectorGameMain
+{
+
+	public static void main(String[] args)
+	{
+		
+		//ThisFileCanBeHarmfulWarning
+		
+		//VectorIntroduction
+		
+		//MinimizeWindows
+		
+		//MoveMouse
+		
+		//OpenWindows
+		
+		//TypeInNotepad
+		
+		//UndefeatableSpeech
+		
+		//PastComputers
+		
+		//WeaknessPopup(WithMouseLock)
+		
+		//ClickTheWindowToEnd(WithVectorsPowers)
+		
+		//Shutdown
+		
+	}
+	
+}
